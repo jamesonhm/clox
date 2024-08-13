9a27b652d5421e4dc248b2205c03176fb384d455
jamesonhm <jamesonhmarshall@gmail.com>
2024-08-13 06:52:25 -0600

    ch14 initial commit

---
+++ chunk.h
@@ -0,0 +1,20 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN, //return from the current function
+} OpCode;
+
+// This is a dynamic array...?
+// count is the number of entries in use
+// capacity is the number of elements allocated
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+#endif 
+
