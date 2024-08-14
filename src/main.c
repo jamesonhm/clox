dec2aa6c11246408af5c868d464174d65bc25581
jamesonhm <jamesonhmarshall@gmail.com>
2024-08-14 14:13:03 -0600

    ch14 start of disassembler

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
+#include <stdio.h>
+
 #include "chunk.h"
-#include "common.h"
+// #include "common.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
+    printf("chunk initialized\n");
     writeChunk(&chunk, OP_RETURN);
+    printf("chunk written\n");
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
+    printf("chunk freed\n");
     return 0;
 }
 
