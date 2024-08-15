bf4c4b182929f3f38eda8b7cfe9a74178f20f88f
jamesonhm <jamesonhmarshall@gmail.com>
2024-08-15 06:29:43 -0600

    ch14 constants

--- src/chunk.h
+++ src/chunk.h
@@ -2,8 +2,10 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT, //produce a particular constant
     OP_RETURN, //return from the current function
 } OpCode;
 
@@ -14,11 +16,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif 
 
