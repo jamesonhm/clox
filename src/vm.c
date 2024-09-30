55d0237cddf2ecd9897ee8d775889aa2a0748fd5
jamesonhm <jamesonhmarshall@gmail.com>
2024-09-30 06:02:50 -0600

    ch24 complete

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,6 +13,10 @@
 #include "vm.h"
 
 VM vm;
+
+static Value clockNative(int argCount, Value* args) {
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 static void resetStack() {
     vm.stackTop = vm.stack;
@@ -54,6 +59,8 @@ void initVM() {
 
     initTable(&vm.globals);
     initTable(&vm.strings);
+
+    defineNative("clock", clockNative);
 }
 
 void freeVM() {
